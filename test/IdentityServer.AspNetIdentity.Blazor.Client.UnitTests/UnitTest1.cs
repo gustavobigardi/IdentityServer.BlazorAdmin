401cf8bc9c87bf7fde15d36d410a5a9f05489d40
Gustavo Bellini Bigardi <gbbigardi@gmail.com>
2019-02-18 16:54:54 -0300

    Adding test projects, but only structure.

---
+++ test/IdentityServer.AspNetIdentity.Blazor.Client.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace IdentityServer.AspNetIdentity.Blazor.Client.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
